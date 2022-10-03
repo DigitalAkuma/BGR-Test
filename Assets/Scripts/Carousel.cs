b67058dcfbac90988b989b56ba9f6ec959fc51fb
natilyy <57195846+natilyy@users.noreply.github.com>
2022-10-03 04:15:27 +0100

    carousel finished

--- Assets/Scripts/Carousel.cs
+++ Assets/Scripts/Carousel.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,19 +13,22 @@ public class Carousel
     public int currentPos; //which number is in the start position
     public List<Button> moveButtons;
     public List<Button> slotButtons;
-    public List<Dictionary<string, Texture2D>> Assets;
+    public List<Dictionary<string, Sprite>> Assets; //list of dictionaries containign assets ORDERED
+    public GameObject selected; //the gameobject of the selection image
+    public int selectedAsset; //which asset is selected;
 
-    public Carousel(GameObject wheel, int resourceCount, int slotCount)
+    public Carousel(GameObject wheel, int slotCount)
     {
         string[] splits = wheel.name.Split('_');
 
         this.name = splits[0];
         this.wheel = wheel;
         this.slotCount = slotCount;
-        this.resourceCount = resourceCount;
         this.currentPos = 1;
+        this.selected = wheel.transform.Find("selected").gameObject;
+        this.selectedAsset = 0;
 
-        Assets = new List<Dictionary<string, Texture2D>>();
+        Assets = new List<Dictionary<string, Sprite>>();
 
         //loop through each folder in relevant asset folder, add to dictionary 
         //keeps going until no more assets can be found
@@ -33,15 +37,15 @@ public class Carousel
 
         while (valid == true)
         {
-            Texture2D[] allResources = Resources.LoadAll<Texture2D>("DressUpAssets/" + name + "/" + name + i);
+            Sprite[] allResources = Resources.LoadAll<Sprite>("DressUpAssets/" + name + "/" + name + i);
             if (allResources != null && allResources.Length != 0)
             {
-                Dictionary<string, Texture2D> currentDict = new Dictionary<string, Texture2D>();
-                foreach (Texture2D texture in allResources)
+                Dictionary<string, Sprite> currentDict = new Dictionary<string, Sprite>();
+                foreach (Sprite texture in allResources)
                 {
                     if (texture.name.Contains("Thumbnail"))
                     {
-                        currentDict.Add("Thumbnail", texture);
+                        currentDict.Add("thumbnail", texture);
                     }
                     else
                     {
@@ -57,7 +61,7 @@ public class Carousel
             }
         }
 
-        Debug.Log(Assets.Count);
+        this.resourceCount = i - 1;
 
         moveButtons = new List<Button>();
         slotButtons = new List<Button>();
@@ -66,13 +70,14 @@ public class Carousel
         {
             if (child.gameObject.GetComponent<Button>()) 
             {
-                //adding listeners to move buttons (left, right, down)
+                //adding listeners to move buttons (left, right, down, up)
                 if (child.name.Contains("bt_"))
                 {
                     Button childButton = child.gameObject.GetComponent<Button>();
                     moveButtons.Add(childButton);
                     childButton.onClick.AddListener(() => MoveButtonClicked(childButton));
                 }
+                DisableBackButton();
                 //adding listeners to thumbnail slots
                 if (child.name.Contains("slot"))
                 {
@@ -100,19 +105,100 @@ public class Carousel
 
     void SlotButtonClicked(Button myButton)
     {
-        Debug.Log(myButton);
-        //CHANGE DICTIONARY IN DRESSUPSETTINGS
+        if (myButton.gameObject.transform.Find("selected"))
+        {
+            selected.transform.SetParent(myButton.gameObject.transform.parent);
+            selected.GetComponent<Image>().enabled = false;
+            selectedAsset = 0;
+        }
+        else {
+            selected.transform.SetParent(myButton.gameObject.transform);
+            selected.transform.localPosition = new Vector3(-1, -1, 0);
+            selected.GetComponent<Image>().enabled = true;
+            string buttonName = myButton.gameObject.name;
+            int slotNumber = (int)Char.GetNumericValue(buttonName[buttonName.Length - 1]);
+            selectedAsset = currentPos - 1 + slotNumber;
+        }     
     }
 
     void AdvanceCarousel()
     {
         //check if max numbers, if so set to 1
-        
+        if (currentPos + slotCount - 1 == resourceCount)
+        { 
+            currentPos = 1;
+            DisableBackButton();
+        }
+        else 
+        { 
+            currentPos++;
+            EnableBackButton();
+        }
+
+        int count = -1;
+        foreach (Button bt in slotButtons)
+        {
+            bt.gameObject.GetComponent<Image>().sprite = Assets[currentPos + count]["thumbnail"];
+            if (bt.gameObject.transform.Find("selected"))
+            {
+                selected.transform.SetParent(bt.gameObject.transform.parent);
+                selected.GetComponent<Image>().enabled = false;
+            }
+            if (currentPos + count + 1 == selectedAsset)
+            {
+                selected.transform.SetParent(bt.gameObject.transform);
+                selected.transform.localPosition = new Vector3(-1, -1, 0);
+                selected.GetComponent<Image>().enabled = true;
+            }
+            count++;
+        }
     }
 
     void ReverseCarousel()
     {
-        //if on 1, do nothing
+        if (currentPos == 2)
+        {
+            DisableBackButton();
+        }
+        currentPos--;
+
+        int count = -1;
+        foreach (Button bt in slotButtons)
+        {
+            bt.gameObject.GetComponent<Image>().sprite = Assets[currentPos + count]["thumbnail"];
+            if (bt.gameObject.transform.Find("selected"))
+            {
+                selected.transform.SetParent(bt.gameObject.transform.parent);
+                selected.GetComponent<Image>().enabled = false;
+            }
+            if (currentPos + count + 1 == selectedAsset)
+            {
+                selected.transform.SetParent(bt.gameObject.transform);
+                selected.transform.localPosition = new Vector3(-1, -1, 0);
+                selected.GetComponent<Image>().enabled = true;
+            }
+            count++;
+        }
+    }
+
+    void DisableBackButton()
+    {
+        foreach (Button bt in moveButtons)
+        {
+            GameObject btObj = bt.gameObject;
+            if (btObj.name.Contains("left") || btObj.name.Contains("up"))
+                btObj.GetComponent<Image>().enabled = false; //DISABLES BACK BUTTON
+        }
+    }
+
+    void EnableBackButton()
+    {
+        foreach (Button bt in moveButtons)
+        {
+            GameObject btObj = bt.gameObject;
+            if (btObj.name.Contains("left") || btObj.name.Contains("up"))
+                btObj.GetComponent<Image>().enabled = true; //ENABLES BACK BUTTON
+        }
     }
 }
 
--- Assets/Scripts/applyClothing.cs
+++ Assets/Scripts/applyClothing.cs
@@ -6,37 +6,12 @@ using UnityEngine.EventSystems;
 
 public class ApplyClothing : MonoBehaviour
 {
-    public Carousel hair_carousel;
 
     void Start()
     {
-        hair_carousel = new Carousel(GameObject.Find("hair_carousel"), 15, 4);
+        Carousel hair_carousel = new Carousel(GameObject.Find("hair_carousel"), 4);
+        Carousel lips_carousel = new Carousel(GameObject.Find("lips_carousel"), 2);
+        Carousel eyes_carousel = new Carousel(GameObject.Find("eyes_carousel"), 2);
     }
 
-    public void MoveCarousel()
-    {
-        GameObject btn = EventSystem.current.currentSelectedGameObject;
-        //split name to get direction (buttons name bt_left, bt_right, bt_down)
-        string[] splits = btn.name.Split('_');
-        string direction = splits[1];
-        //Debug.Log(direction);
-
-        //get which carousel its for
-        GameObject btnParent = btn.transform.parent.gameObject;
-        //Debug.Log(btnParent.name);
-
-        //find the actual carousel
-        GameObject carousel = GameObject.Find(btnParent.name + "_carousel");
-        //Debug.Log(carousel.name);
-
-        if (direction == "right" || direction == "down")
-        {
-            //AdvanceCarousel(carousel);
-        }
-        else
-        {
-            // ReverseCarousel(carousel);
-        }
-
-    }
 }
